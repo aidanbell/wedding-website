d6de8f2b86b2fde41b6b1dc617c5fbb2d9f25249
Aidan Bell <aidanbell0182@gmail.com>
2023-10-11 13:29:08 -0400

    more work on RSVP form

--- src/pages/RSVP/RSVP.tsx
+++ src/pages/RSVP/RSVP.tsx
@@ -1,6 +1,6 @@
 // FORM DATA:
 // - Name(s) (all guests on your invite) (string)
-    // - + names
+// - + names
 // - Email (main contact email just in case) (string)
 // - RSVP (bool)
 // - Food (?) (string)
@@ -10,14 +10,14 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
-import "./RSVP.css"
+import "./RSVP.css";
 
 export default function RSVP() {
   const [formData, setFormData] = useState({
     name: "",
     names: [],
     email: "",
-    rsvp: "",
+    rsvp: true,
     food: "",
     dietaryRestrictions: "",
     accessibilityNeeds: "",
@@ -45,7 +45,16 @@ export default function RSVP() {
       setFormData({ ...formData, names: newNames });
       return;
     }
+    if (event.target.name === "rsvp") {
+      setFormData({ ...formData, rsvp: event.target.value === "true" });
+      return;
+    }
     setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(formData);
   }
 
   return (
@@ -58,43 +67,125 @@ export default function RSVP() {
       }}
       exit={{ opacity: 0 }}>
       <h1>RSVP</h1>
-      <form className="rsvp-form">
-        <label htmlFor="names">
+      <p>Let us know if we'll see you there! To add additional names, press the '+' button</p>
+      <form className="rsvp-form" onSubmit={handleSubmit}>
+        <label className="name" htmlFor="name">
           Name(s):
-          <input type="text" id="names" name="names" />
-          <button onClick={additionalNames}>+</button>
         </label>
-        {formData.names.map((name, i) => {
-          return (
-            <label htmlFor="names">
-              :
-              <input type="text" id={`name-${i}`} name="names" onChange={handleFormChange}/>
-              <button onClick={removeName}>-</button>
-            </label>
-          )
+        <div className="name">
+          <div className="main-name">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleFormChange}
+            />
+            <button onClick={additionalNames}>+</button>
+          </div>
+          {formData.names.map((name, i) => {
+            return (
+              <div className="more-names">
+                <input
+                  type="text"
+                  name="names"
+                  id={`name-${i}`}
+                  value={formData.names[i]}
+                  onChange={handleFormChange}
+                />
+                <button className="remove" id={`namebtn-${i}`} onClick={removeName}>
+                  -
+                </button>
+              </div>
+            );
           })}
+        </div>
         <label htmlFor="email">
           Email:
-          <input type="email" id="email" name="email" />
+          <br />
+          <span className="small">Primary Contact</span>
         </label>
-        <label htmlFor="rsvp">
-          RSVP:
-          <input type="checkbox" id="rsvp" name="rsvp" />
-        </label>
+        <input
+          className="email"
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleFormChange}
+        />
+        <label htmlFor="rsvp">RSVP:</label>
+        <div className="rsvp">
+          <label htmlFor="yes">
+            <input
+              type="radio"
+              id="yes"
+              name="rsvp"
+              value="true"
+              checked={formData.rsvp}
+              onChange={handleFormChange}
+            />
+            {formData.names.length ? "We'll" : "I'll"} be there
+          </label>
+          <label htmlFor="no">
+            <input 
+              type="radio" 
+              id="no" 
+              name="rsvp"
+              value="false"
+              checked={!formData.rsvp}
+              onChange={handleFormChange}
+              />
+            Sorry, {formData.names.length ? "We" : "I"} can't make it
+          </label>
+        </div>
         <label htmlFor="food">
           Food:
-          <input type="text" id="food" name="food" />
+          <br />
+          <span className="small">More info coming soon!</span>
         </label>
-        <label htmlFor="dietaryRestrictions">Dietary Restrictions:</label>
+        <select id="food" name="food" onChange={handleFormChange}>
+          <option value="chicken">Chicken</option>
+          <option value="beef">Beef</option>
+          <option value="seafood">Seafood</option>
+          <option value="vegetarian">Vegetarian</option>
+        </select>
+        <label htmlFor="dietaryRestrictions">
+          Dietary Restrictions:
+          <br />
+          <span className="small">Let us know!</span>
+        </label>
         <input
           type="text"
           id="dietaryRestrictions"
           name="dietaryRestrictions"
+          value={formData.dietaryRestrictions}
+          onChange={handleFormChange}
         />
-        <label htmlFor="accessibilityNeeds">Accessibility Needs:</label>
-        <input type="text" id="accessibilityNeeds" name="accessibilityNeeds" />
-        <label htmlFor="message">Message for Bride & Groom:</label>
-        <textarea id="message" name="message" />
+        <label htmlFor="accessibilityNeeds">
+          Accessibility Needs:
+          <br />
+          <span className="small">
+            Anything we can do to make you more comfortable
+          </span>
+        </label>
+        <input 
+          type="text" 
+          id="accessibilityNeeds" 
+          name="accessibilityNeeds"
+          value={formData.accessibilityNeeds}
+          onChange={handleFormChange}
+          />
+        <label htmlFor="message">
+          Message for Bride & Groom:
+          <br />
+          <span className="small">Optional, but welcome :)</span>
+        </label>
+        <textarea 
+          id="message" 
+          name="message"
+          value={formData.message}
+          onChange={handleFormChange}
+          />
         <input type="submit" value="Submit" />
       </form>
     </motion.div>
